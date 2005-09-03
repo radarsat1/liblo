1da272a6eecccd6359b12aefe99adad544687442
theno23 <theno23@8a0476ba-7714-0410-bd95-fb3ea4a72f84>
2005-09-03 12:21:52 +0000

    close address socket on _free()
    git-svn-id: https://liblo.svn.sourceforge.net/svnroot/liblo/trunk@49 8a0476ba-7714-0410-bd95-fb3ea4a72f84

--- src/address.c
+++ src/address.c
@@ -132,6 +132,9 @@ char *lo_address_get_url(lo_address a)
 void lo_address_free(lo_address a)
 {
     if (a) {
+	if (a->socket != -1) {
+	    close(a->socket);
+	}
 	free(a->host);
 	free(a->port);
 	if (a->ai && a->ai != (void *)1) {
