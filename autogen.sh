02a21feb24ae844632e015964836c7a1646aef02
nhumfrey <nhumfrey@8a0476ba-7714-0410-bd95-fb3ea4a72f84>
2005-12-05 19:03:32 +0000

    Think we should be checking for libtoolize here
    git-svn-id: https://liblo.svn.sourceforge.net/svnroot/liblo/trunk@51 8a0476ba-7714-0410-bd95-fb3ea4a72f84

--- autogen.sh
+++ autogen.sh
@@ -39,7 +39,7 @@ fi
 }
 
 (grep "^AM_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
+  (libtoolize --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have \`libtool' installed."
     echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
