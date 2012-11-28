b1d2d06323a728f4b17427dad96ac4168be722a3
Camille Troillard <camille@osculator.net>
2012-11-28 12:16:48 +0100

    ensure we compare integers of same signedness.

--- src/bundle.c
+++ src/bundle.c
@@ -57,7 +57,7 @@ int lo_bundle_add_message(lo_bundle b, const char *path, lo_message m)
 lo_message lo_bundle_get_message(lo_bundle b, int index,
                                  const char **path)
 {
-    if (index < b->len) {
+    if (index < (int)b->len) {
         if (path)
             *path = b->paths[index];
         return b->msgs[index];
